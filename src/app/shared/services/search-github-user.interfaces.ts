757b130cf601ea144a2e2b4d5e2595b14fcf4825
Matheus Rocha <im.rochamatheus@gmail.com>
2023-11-19 16:43:33 -0300

    :truck: refactor: alterar diretório do serviço de busca de perfis

--- src/app/shared/services/search-github-user.interfaces.ts
+++ src/app/shared/services/search-github-user.interfaces.ts
@@ -2,7 +2,6 @@ export interface GithubUser {
   id: number;
   url: string;
   bio: string;
-  type: string;
   name: string;
   blog: string;
   email: string;
@@ -10,16 +9,9 @@ export interface GithubUser {
   avatar_url: string;
   company?: string;
   location: string;
-  hireable: string;
-  repos_url: string;
   followers: number;
   following: number;
   created_at: string;
-  updated_at: string;
-  public_gists: number;
   public_repos: number;
-  following_url: string;
-  followers_url: string;
   twitter_username: string;
-  organizations_url: string;
 }
