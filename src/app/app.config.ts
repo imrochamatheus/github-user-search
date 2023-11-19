172cf29a225ccfc1e91e046838cb64d3024fb421
Matheus Rocha <im.rochamatheus@gmail.com>
2023-11-18 23:46:16 -0300

    :sparkles: feat: adicionar módulo http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
+
+import pt from '@angular/common/locales/pt';
+
 import { routes } from './app.routes';
 
+registerLocaleData(pt);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient()],
 };
